de14f4ea7cf4f2a40ed9056f900e437e1e897c5d
vilitin <36882320+vilitin@users.noreply.github.com>
2018-02-27 20:02:12 +0300

    Update main.py

--- datmusic/main.py
+++ datmusic/main.py
@@ -45,7 +45,8 @@ def inlinequery(bot, update):
                     )
                 )
             update.inline_query.answer(results=results, cache_time=INLINE_QUERY_CACHE_TIME)
-except Exception as e
+try:
+ except Exception as e:
 error(bot, update, e)
 
 def search(query):
